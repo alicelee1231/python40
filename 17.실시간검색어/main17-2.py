0624fe5693e79167a625e80ae99116b6bc181d86
alicelee1231 <saemmilee1231@gmail.com>
2023-11-08 22:08:39 +0900

    fixing completed

--- 17.실시간검색어/main17-2.py
+++ 17.실시간검색어/main17-2.py
@@ -9,11 +9,11 @@ URL = 'https://zum.com'
 driver.get(url = URL)
 driver.implicitly_wait(time_to_wait=10)
 
-driver.find_element(By.CSS_SELECTOR,'/html/body/ntp-app//div/div[2]/ntp-realbox//div/input').send_keys("아무거나")
-time.sleep(0.5)
+# driver.find_element(By.CSS_SELECTOR,'#search-input').send_keys("아무거나")
+# time.sleep(0.5)
 
-driver.find_element(By.CSS_SELECTOR,'#icon').click()
-time.sleep(1)
+# driver.find_element(By.CSS_SELECTOR,'#app > div > header > div.search_bar > div > fieldset > div > button.search').click()
+# time.sleep(1)
 
 zoom_results= driver.find_elements(By.CSS_SELECTOR,'#app > div > header > div.gnb_wrap > div > div > div.issue_wrap > div > div.list_wrap.issue_type')
 
